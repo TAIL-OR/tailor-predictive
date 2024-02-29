099acb7e3299c9863f0966bcc38c17b3428e8d99
Franky03 <kaikym20@gmail.com>
2024-02-29 01:14:08 -0300

    Add xgboost_2 plots

--- scripts/plot_future.py
+++ scripts/plot_future.py
@@ -22,12 +22,12 @@ future_data_delphi['date'] = pd.to_datetime(future_data_delphi['date'])
 past_data_last_4_months = past_data[past_data['date'] >= past_data['date'].max() - pd.DateOffset(months=4)]
 
 # Plot the data
-output_path = '../models/plots/delphi/'
+output_path = '../models/plots/xgboost_2/'
 for ra in past_data_last_4_months['ra'].unique():
     plt.plot(past_data_last_4_months[past_data_last_4_months['ra'] == ra]['date'], past_data_last_4_months[past_data_last_4_months['ra'] == ra]['case_cnt'], color='blue', label='Past Data')
-    #plt.plot(future_data[future_data['ra'] == ra]['date'], future_data[future_data['ra'] == ra]['case_cnt'], color='red', label='Future Data')
+    plt.plot(future_data[future_data['ra'] == ra]['date'], future_data[future_data['ra'] == ra]['case_cnt'], color='red', label='Future Data')
     #plt.plot(future_data_lgbm[future_data_lgbm['ra'] == ra]['date'], future_data_lgbm[future_data_lgbm['ra'] == ra]['case_cnt'], color='purple', label='Future Data LGBM')
-    plt.plot(future_data_delphi[future_data_delphi['ra'] == ra]['date'], future_data_delphi[future_data_delphi['ra'] == ra]['case_cnt'], color='green', label='Future Data Delphi')
+    #plt.plot(future_data_delphi[future_data_delphi['ra'] == ra]['date'], future_data_delphi[future_data_delphi['ra'] == ra]['case_cnt'], color='green', label='Future Data Delphi')
     plt.xlabel('Time')
     plt.ylabel('Case Count')
     plt.title(f'RA {ra} - Past (last 4 months) and Future Case Count')
